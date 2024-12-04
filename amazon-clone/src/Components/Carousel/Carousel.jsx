2ff6769de5b599417c2a16b759e720e9dc755326
Kewanim <61210137+kewanim@users.noreply.github.com>
2024-12-04 17:41:53 -0500

    Finished building Authentication Functionality using Video 3.4

--- amazon-clone/src/Components/Carousel/Carousel.jsx
+++ amazon-clone/src/Components/Carousel/Carousel.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
-import {img} from './img/data'
-import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
-import classes from './../Carousel/Carousel.module.css'
+// import React from 'react'
+// import {Carousel} from 'react-responsive-carousel'
+// import {img} from './img/data'
+// import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+// import classes from './../Carousel/Carousel.module.css'
 
 
 
-function CarouselEffect() {
-    return (
-        <div>
-            <Carousel
-                autoPlay={true}
-                infiniteLoop={true}
-                showIndicators={false}
-                showThumbs={false}        
-            >
-                {
-                    img.map((imageItemLink)=>{
-                        return <img src={imageItemLink} />
+// function CarouselEffect() {
+//     return (
+//         <div>
+//             <Carousel
+//                 autoPlay={true}
+//                 infiniteLoop={true}
+//                 showIndicators={false}
+//                 showThumbs={false}        
+//             >
+//                 {
+//                     img.map((imageItemLink)=>{
+//                         return <img key={ imageItemLink} src={imageItemLink} />
 
-                        //added key={index} alt={`Carousel Item ${index}`}//
-                    })  
-                }
+//                         //added key={index} alt={`Carousel Item ${index}`}//
+//                     })  
+//                 }
 
-            </Carousel>
-                <div className={classes.hero__img}></div>
-        </div>
-    )
-}
+//             </Carousel>
+//                 <div className={classes.hero__img}></div>
+//         </div>
+//     )
+// }
 
-export default CarouselEffect;
+// export default CarouselEffect;
 
 //------------------------------------------------------------------------
 // import React from 'react'
@@ -98,3 +98,40 @@ export default CarouselEffect;
 // }
 
 // export default CarouselEffect;
+
+//-----------------------------
+
+
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import { img } from './img/data'
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+import classes from './../Carousel/Carousel.module.css'
+
+function CarouselEffect() {
+    return (
+        <div>
+            <Carousel
+                autoPlay={true}
+                infiniteLoop={true}
+                showIndicators={false}
+                showThumbs={false}        
+            >
+                {
+                    img.map((imageItemLink, index) => {
+                        return (
+                            <img 
+                                key={index} 
+                                src={imageItemLink} 
+                                alt={`Carousel Item ${index}`} 
+                            />
+                        )
+                    })  
+                }
+            </Carousel>
+            <div className={classes.hero__img}></div>
+        </div>
+    )
+}
+
+export default CarouselEffect;
--- amazon-clone/src/Pages/Auth/Auth.jsx
+++ amazon-clone/src/Pages/Auth/Auth.jsx
@@ -1,45 +1,206 @@
-import React from 'react'
-import classes from './SignUp.module.css'
-import { Link } from 'react-router-dom'
+// import React, { useState, useContext } from 'react'
+// import classes from './SignUp.module.css'
+// import { Link } from 'react-router-dom'
+// import { auth } from '../../Utility/firebase';
+// import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth"
+// import {DataContext} from '../../Components/Data Provider/DataProvide'
 
 
+// function Auth() {
 
+//     const[email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [error, setError] = useState("");
+
+//     const [{user}, dispatch] = useContext(DataContext)
+
+//     const authHandler = async(e)=>{
+//         e.preventDefault();
+//         console.log(e.target.name);
+//         if(e.target.name == "signin"){
+//             // firebase auth
+//             signInWithEmailAndPassword(auth, email, password)
+//             .then((userInfo)=>{
+//             console.log(userInfo);
+//             dispatch({
+//                 type:Type.SET_USER,
+//                 user:userInfo.user
+//             })
+//         }).catch((err)=>{
+//             console.log(err)
+//         })
+//         }else{
+//             createUserWithEmailAndPassword (auth, email, password)
+//             .then ((userInfo)=>{
+//                 console.log(userInfo);
+//                 dispatch({
+//                     type:Type.SET_USER,
+//                     user:userInfo.user
+//             })
+//             .catch((err)=>{
+//                 console.log(err);
+//             });
+//         }
+//     };
+    
+
+
+//     // console.log(password, email);
+
+//     return <section className={classes.login}>
+//                     {/* Logo */}
+//                 <Link>
+//                     <img 
+//                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" 
+//                     alt=""/>
+//                 </Link>
+
+//                     {/* Form */}
+//                     <div className={classes.login_container}>
+//                         <h1>Sign In</h1>
+//                         <form action="">
+//                             <div>
+//                                 <label htmlFor="email">Email</label>
+//                                 <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" id="email" />
+//                             </div>
+//                             <div>
+//                                 <label htmlFor="password">Password</label>
+//                                 <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" id="password" />
+//                             </div>
+//                             <button type="submit" 
+//                             onClick={authHandler} 
+//                             name="signin"
+//                             className={classes.login_signInButton}>
+//                                 Sign In
+//                             </button>
+//                         </form>
+
+//                         {/* Agreement */}
+//                         <p>
+//                             By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interes-Based Ads Notice.
+//                         </p>
+
+//                         {/* Create Account Button */}
+//                         <button type="submit" 
+//                         onClick={authHandler} 
+//                         name="signup"
+//                         className={classes.login_registerButton}>
+//                             Create a new Amazon Account
+//                         </button>
+//                     </div>
+//             </section>
+// };
+
+// export default Auth
+
+import React, { useState, useContext } from 'react';
+import classes from './SignUp.module.css';
+import { Link } from 'react-router-dom';
+import { auth } from '../../Utility/firebase';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { DataContext } from '../../Components/Data Provider/DataProvide';
 
 function Auth() {
-    return <section className={classes.login}>
-                    {/* Logo */}
-                <Link>
-                    <img 
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" 
-                    alt=""/>
-                </Link>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-                    {/* Form */}
-                    <div className={classes.login_container}>
-                        <h1>Sign In</h1>
-                        <form action="">
-                            <div>
-                                <label htmlFor="email">Email</label>
-                                <input type="email" id="email" />
-                            </div>
-                            <div>
-                                <label htmlFor="password">Password</label>
-                                <input type="password" id="password" />
-                            </div>
-                            <button className={classes.login_signInButton}>Sign In</button>
-                        </form>
+    const [{ user }, dispatch] = useContext(DataContext); // Ensure `user` is imported correctly
 
-                        {/* Agreement */}
-                        <p>
-                            By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interes-Based Ads Notice.
-                        </p>
+console.log(user)
 
-                        {/* Create Account Button */}
-                        <button className={classes.login_registerButton}>
-                            Create a new Amazon Account
-                        </button>
+    const authHandler = async (e) => {
+        e.preventDefault();
+        console.log(e.target.name);
+
+        if (e.target.name === "signin") { // FIX: Use strict equality operator (===)
+            // Firebase auth
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userInfo) => {
+                    dispatch({
+                        type: "SET_USER", // Ensure "SET_USER" matches your action type definition
+                        user: userInfo.user,
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setError("Failed to sign in. Please try again.");
+                });
+        } else {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userInfo) => {
+                    dispatch({
+                        type: "SET_USER", // Ensure "SET_USER" matches your action type definition
+                        user: userInfo.user,
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setError("Failed to create an account. Please try again.");
+                });
+        }
+    };
+
+    return (
+        <section className={classes.login}>
+            {/* Logo */}
+            <Link to="/">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    alt="Amazon Logo"
+                />
+            </Link>
+
+            {/* Form */}
+            <div className={classes.login_container}>
+                <h1>Sign In</h1>
+                {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error if exists */}
+                <form action="">
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            id="email"
+                        />
                     </div>
-            </section>
-};
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            id="password"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        onClick={authHandler}
+                        name="signin"
+                        className={classes.login_signInButton}
+                    >
+                        Sign In
+                    </button>
+                </form>
 
-export default Auth
+                {/* Agreement */}
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                {/* Create Account Button */}
+                <button
+                    type="submit"
+                    onClick={authHandler}
+                    name="signup"
+                    className={classes.login_registerButton}
+                >
+                    Create a new Amazon Account
+                </button>
+            </div>
+        </section>
+    );
+}
+
+export default Auth;
