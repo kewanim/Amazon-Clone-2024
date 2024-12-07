f645be17e171b43b1edfd3577a8bf124c8704673
Kewanim <61210137+kewanim@users.noreply.github.com>
2024-12-07 13:32:17 -0500

    Finsihed Route Protection --- using Video 3.12

--- amazon-clone/src/Router.jsx
+++ amazon-clone/src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, redirect} from "react-router-dom"
 import Landing from './Pages/Landing/Landing'
 import Auth from './Pages/Auth/Auth'
 import Payment from './Pages/Payment/Payment'
@@ -9,6 +9,7 @@ import Results from './Pages/Results/Results'
 import ProductDetail from './Pages/ProductDetail/ProductDetail'
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute'
 
 const stripePromise = loadStripe('pk_test_51QSWeuGLLvNzOpAjTK3QHFMo4tbJmdtKuMMOG7VDIu1sZQKVmvEqj12GSqavjHWm2rO4kR2UZSUqZFV5HXF33FvD00je6joK4d');
 
@@ -19,11 +20,23 @@ function Routing() {
                 <Route path="/" element={<Landing />}/>
                 <Route path="/auth" element={<Auth />}/>
                 <Route path="/payments" element={
-                    <Elements stripe={stripePromise}>
-                        <Payment />
-                    </Elements>
+                    <ProtectedRoute 
+                        msg={"You must log in to pay"} 
+                        redirect={"/payments"}
+                    >
+                            <Elements stripe={stripePromise}>
+                                <Payment />
+                            </Elements>
+                    </ProtectedRoute>
                 }/>
-                <Route path="/orders" element={<Orders />}/>
+                <Route path="/orders" element={
+                    <ProtectedRoute 
+                        msg={"You must log in to access your orders"} 
+                        redirect={"/orders"}
+                        >
+                            <Orders />
+                    </ProtectedRoute>
+                    }/>
                 <Route path="/category/:categoryName" element={<Results/>} />
                 <Route path="/products/:productId" element={<ProductDetail/>}/>
                 <Route path="cart" element={<Cart/>}/>
