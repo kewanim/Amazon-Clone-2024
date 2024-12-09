40d8631c6c6e7cf5524d87504eda9df6b173941a
Kewanim <61210137+kewanim@users.noreply.github.com>
2024-12-09 13:11:37 -0500

    Completed Project

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-38cff/us-central1/api",
+    // baseURL: "http://127.0.0.1:5001/clone-38cff/us-central1/api",
+    baseURL:"https://us-central1-clone-38cff.cloudfunctions.net/api",
     responseType: "json", // Default for APIs; change to 'text' or 'arraybuffer' if needed
 });
 
